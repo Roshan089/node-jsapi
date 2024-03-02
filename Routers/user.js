702965a865d9543b23b6bb0693fbc70bfa5c6428
Roshan <1js20is089@gmail.com>
2024-03-02 13:39:16 +0530

    model/routers

---
+++ Routers/user.js
@@ -0,0 +1,36 @@
+
+import express from "express"
+import { User } from "../models/user.js ";
+
+
+const router = express.Router()
+
+router.get("/all", async(req, res) => {
+    
+  const users=  await User.find({ })
+    
+
+    res.json({
+        success: true,
+        users,
+    })  
+}) 
+
+router.post("/new", async(req, res) => {
+    
+    const { name, email, password } = req.body;
+
+    await User.create({
+        name,
+        email,
+        password
+   })
+    
+
+    res.json({
+        success: true,
+        message: "regestered success"
+    })  
+}) 
+
+export default router
--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from "express"
 import path from "path"
 import mongoose from "mongoose"
+import userRouter from "./Routers/user.js"
 
 const app = express();
+
+//using middlewere
+app.use(express.json())
+app.use("/user", userRouter)
 
 mongoose.connect("mongodb://localhost:27017", {
     dbName: "nodejsapi"
@@ -10,17 +15,11 @@ mongoose.connect("mongodb://localhost:27017", {
 .catch((e) => console.log(e))
 
 
+
 app.get("/", (req,res) => {
     res.send("nice working");
 
 })
-app.get("/user/all", (req,res) => {
-    res.json({
-        success: true,
-        users:[],
-    })  
-}) 
-
 app.listen(4000, () => {
     console.log("server is working")
 })
